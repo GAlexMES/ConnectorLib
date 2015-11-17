e9ed8cce05d48c63ae3a04adc36b74d29e34fced
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-11-17 15:57:58 +0100

    changed relative path to absolute path
    .class.getResource(".") is not possible, when the project is packed as a
    .jar file

--- src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messages/HandshakeMapperCreator.java
+++ src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messages/HandshakeMapperCreator.java
@@ -1,14 +1,11 @@
 package de.szut.dqi12.cheftrainer.connectorlib.messages;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
-import de.szut.dqi12.cheftrainer.connectorlib.callables.CallableController;
 import de.szut.dqi12.cheftrainer.connectorlib.messageids.Handshake_MessageIDs;
 
 public class HandshakeMapperCreator {
-	
+
 	/**
 	 * 
 	 * @return a new IDClass_Path_Mapper for the Handshake Message IDs
@@ -16,17 +13,13 @@ public class HandshakeMapperCreator {
 	public static IDClass_Path_Mapper getIDClassPathMapperForHandshake() {
 
 		Handshake_MessageIDs hm_messageIDs = new Handshake_MessageIDs();
-		URL pathToHandshakeDir = null;
-		try {
-			pathToHandshakeDir = CallableController.class.getResource(".")
-					.toURI().toURL();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		String pathToHandshakePackage = CallableController.class.getName();
 		
+		URL pathToHandshakeDir = HandshakeMapperCreator.class.getResource("/de/szut/dqi12/cheftrainer/connectorlib/callables");
+		System.out.println(pathToHandshakeDir.toString());
+		
+		String pathToHandshakePackage = "de.szut.dqi12.cheftrainer.connectorlib.callables.CallableController";
+		System.out.println(pathToHandshakePackage.toString());
+
 		return new IDClass_Path_Mapper(hm_messageIDs, pathToHandshakeDir, pathToHandshakePackage);
 	}
 
