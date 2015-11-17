fd2b5a327610ddb9f52a20232f7001ab8098116b
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-11-17 11:26:21 +0100

    added new client to server message id

--- src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messageids/ClientToServer_MessageIDs.java
+++ src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messageids/ClientToServer_MessageIDs.java
@@ -10,4 +10,5 @@ public class ClientToServer_MessageIDs extends MessageIDAbstract{
 	public static String USER_AUTHENTIFICATION ="UserAuthentification";
 	public static String COMMUNITY_AUTHENTIFICATION = "CommunityAuthentification";
 	public static String REQUEST_UPDATE = "UpdateRequest";
+	public static String NEW_FORMATION = "NewFormationUpdate";
 }
