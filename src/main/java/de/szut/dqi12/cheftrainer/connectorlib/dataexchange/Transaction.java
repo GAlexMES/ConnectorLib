cd4058f333bf0f9c155fea76c6d6df8c747eca51
Alexander Brennecke <alexander.brenneck@gmail.com>
2015-11-25 17:48:32 +0100

    updated Transaction

--- src/main/java/de/szut/dqi12/cheftrainer/connectorlib/dataexchange/Transaction.java
+++ src/main/java/de/szut/dqi12/cheftrainer/connectorlib/dataexchange/Transaction.java
@@ -7,12 +7,24 @@ package de.szut.dqi12.cheftrainer.connectorlib.dataexchange;
  */
 public class Transaction {
 	
+	//von Robin
 	private Double price;
 	private boolean outgoing;
 	private String tenderer;
 	private String receiver;
 	private Player player;
 	
+	
+	//von Alex
+	private int offeredPrice;
+	private int playerSportalID;
+	private int communityID;
+	private int userID;
+	private int managerID;
+	
+	public Transaction(){}
+	
+	//von Robin
 	public Transaction(Double price, boolean outgoing, String tenderer,
 			String receiver, Player player) {
 		this.price = price;
@@ -21,6 +33,7 @@ public class Transaction {
 		this.receiver = receiver;
 		this.player = player;
 	}
+	
 	public Double getPrice() {
 		return price;
 	}
@@ -36,5 +49,54 @@ public class Transaction {
 	public Player getPlayer() {
 		return player;
 	}
+	
+	//von Alex
+	public Transaction(int price, int playerID, int communityID, int userID){
+		this.offeredPrice = price;
+		this.playerSportalID = playerID;
+		this.communityID = communityID;
+		this.userID = userID;
+	}
 
+	public int getOfferedPrice() {
+		return offeredPrice;
+	}
+
+	public int getPlayerSportalID() {
+		return playerSportalID;
+	}
+
+	public int getCommunityID() {
+		return communityID;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+	
+	public int getManagerID() {
+		return managerID;
+	}
+
+	public void setOfferedPrice(int offeredPrice) {
+		this.offeredPrice = offeredPrice;
+	}
+
+	public void setPlayerSportalID(int playerSportalID) {
+		this.playerSportalID = playerSportalID;
+	}
+
+	public void setCommunityID(int communityID) {
+		this.communityID = communityID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public void setManagerID(int managerID) {
+		this.managerID = managerID;
+	}
+	
+	
 }
