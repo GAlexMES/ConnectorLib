0bf05350885ae5f5db50dfb2fcfc11b67c2038fb
Alexander Brennecke <alexander.brenneck@gmail.com>
2016-02-23 23:44:41 +0100

    refactored PlayerLabel

--- src/main/java/de/szut/dqi12/cheftrainer/connectorlib/dataexchange/PlayerLabel.java
+++ src/main/java/de/szut/dqi12/cheftrainer/connectorlib/dataexchange/PlayerLabel.java
@@ -3,18 +3,60 @@ package de.szut.dqi12.cheftrainer.connectorlib.dataexchange;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 /**
- * A Label which contains required informations and methods for a Player.
- * These informations are the id of the Player, this position and an image of the Player.
+ * A Label which contains required informations and methods for a Player. These
+ * informations are the id of the Player, this position and an image of the
+ * Player.
+ * 
  * @author Robin
  */
 public class PlayerLabel extends Label {
 	private int id;
+	private VBox content;
 	private Image image;
+	private ImageView iv;
 	private String position;
+	private Player player;
 
-	//GETTER AND SETTER
+	public PlayerLabel() {
+		VBox content = new VBox();
+	}
+
+	public void setImage(Image image) {
+		
+		if(content == null){
+			content = new VBox();
+		}
+		iv = new ImageView();
+		iv.setImage(image);
+		Label nl = new Label(player.getName());
+		Label pl = new Label("("+player.getPoints()+" Punkte)");
+
+		content.setVgrow(iv, Priority.NEVER);
+		content.setVgrow(nl, Priority.NEVER);
+		content.setVgrow(pl, Priority.NEVER);
+		content.getChildren().addAll(iv, nl,pl);
+		this.setGraphic(content);
+		this.image = image;
+	}
+
+	
+	public void setSize(double size) {
+		iv.setFitHeight(size);
+		double newWidth = size/180*150;
+		iv.setFitWidth(newWidth);
+		System.out.println("Image size ="+size+"x"+newWidth);
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	// GETTER AND SETTER
 	public String getPosition() {
 		return position;
 	}
@@ -34,14 +76,4 @@ public class PlayerLabel extends Label {
 	public Image getImage() {
 		return image;
 	}
-
-	public void setImage(Image image) {
-		ImageView iv = new ImageView();
-		iv.fitHeightProperty().bind(this.heightProperty());
-		iv.fitWidthProperty().bind(this.heightProperty());
-		iv.setImage(image);
-		this.setGraphic(iv);
-		this.image = image;
-	}
-
 }
