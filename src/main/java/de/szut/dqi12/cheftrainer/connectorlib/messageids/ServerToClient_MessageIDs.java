57b76898ccdc8533dccbe3059f727d155c6fa95f
Alexander Brennecke <alexander.brenneck@gmail.com>
2016-02-29 21:42:48 +0100

    added one new ID

--- src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messageids/ServerToClient_MessageIDs.java
+++ src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messageids/ServerToClient_MessageIDs.java
@@ -11,5 +11,5 @@ public class ServerToClient_MessageIDs extends MessageIDAbstract {
 	public static String USER_COMMUNITY_LIST = "UserCommunityList";
 	public static String COMMUNITY_AUTHENTICATION_ACK = "CommunityAuthenticationAck";
 	public static String SAVE_FORMATION_ACK = "SaveFormationAck";
-
+	public static String SAVE_OFFER_ACK = "SaveOfferAck";
 }
---
+++ src/main/java/de/szut/dqi12/cheftrainer/connectorlib/messagetemplates/SaveOfferAckMessage.java
@@ -0,0 +1,52 @@
+package de.szut.dqi12.cheftrainer.connectorlib.messagetemplates;
+
+import org.json.JSONObject;
+
+import de.szut.dqi12.cheftrainer.connectorlib.dataexchange.Formation;
+import de.szut.dqi12.cheftrainer.connectorlib.messageids.ServerToClient_MessageIDs;
+
+/**
+ * The SaveFormationAckMessage should be used by the server application to send the acknowledgement for a {@link Formation} change.
+ * @author Alexander Brennecke
+ *
+ */
+public class SaveOfferAckMessage extends MessageTemplate {
+
+	private final static String ID= ServerToClient_MessageIDs.SAVE_OFFER_ACK;
+	private static final String SUCCESFUL = "successful";
+	private boolean successful;
+	
+	/**
+	 * Constructor
+	 * @param successful true= save was successful, false otherwise
+	 */
+	public SaveOfferAckMessage(boolean successful) {
+		super(ID);
+		this.successful=successful;
+	}
+
+	
+	/**
+	 * JSON Constructor
+	 * @param json the {@link JSONObject}. Create it via "createMessageContent()".
+	 */
+	public SaveOfferAckMessage(JSONObject json) {
+		super(ID);
+		successful = json.getBoolean(SUCCESFUL);
+	}
+	
+	
+	@Override
+	public void createMessageContent() {
+		JSONObject json = new JSONObject();
+		json.put(SUCCESFUL, successful);
+		messageContent = json.toString();
+	}
+	
+	//GETTER AND SETTER
+	public boolean isSuccessful(){
+		return successful;
+	}
+
+}
+
